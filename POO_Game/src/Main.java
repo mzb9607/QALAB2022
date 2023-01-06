93dc37dc22098680a9944725a6c24a22e93f72ea
mzb9607 <43447128+mzb9607@users.noreply.github.com>
2023-01-05 20:30:33 -0500

    Cambio en la clase Main de la carpeta POO_Game

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        //Editado por Mauricio Zúñiga
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
         toyo.avanzar();
